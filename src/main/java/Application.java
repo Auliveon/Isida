b738fc8ded56836f81b72be56e95baa3e4726369
Auliveon <62595277+Auliveon@users.noreply.github.com>
2021-07-19 23:04:15 +0300

    commit 2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.annotation.WebServlet;
 
 
 public class Application {
